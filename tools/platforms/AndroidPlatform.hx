9cdb8a6d8c3360eea69fc4a1ff66074ae869db96
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-21 11:07:47 +0000

    Android 7 as min sdk version

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -161,7 +161,7 @@ class AndroidPlatform extends PlatformTarget
 
 		for (architecture in architectures)
 		{
-			var minimumSDKVersion = project.config.getInt("android.minimum-sdk-version", 23);
+			var minimumSDKVersion = project.config.getInt("android.minimum-sdk-version", 24);
 			var haxeParams = [hxml, "-D", "android", "-D", "PLATFORM=android-" + minimumSDKVersion, "-D", "PLATFORM_NUMBER=" + minimumSDKVersion];
 			var cppParams = ["-Dandroid", "-DPLATFORM=android-" + minimumSDKVersion, "-DPLATFORM_NUMBER=" + minimumSDKVersion];
 			var path = sourceSet + "/jniLibs/armeabi";
@@ -374,7 +374,7 @@ class AndroidPlatform extends PlatformTarget
 
 		var commands = [];
 
-		var minimumSDKVersion = project.config.getInt("android.minimum-sdk-version", 23);
+		var minimumSDKVersion = project.config.getInt("android.minimum-sdk-version", 24);
 
 		if (armv5) commands.push(["-Dandroid", "-DPLATFORM=android-" + minimumSDKVersion, "-DPLATFORM_NUMBER=" + minimumSDKVersion]);
 		if (armv7) commands.push(["-Dandroid", "-DHXCPP_ARMV7", "-DPLATFORM=android-" + minimumSDKVersion, "-DPLATFORM_NUMBER=" + minimumSDKVersion]);
@@ -467,7 +467,7 @@ class AndroidPlatform extends PlatformTarget
 		context.CPP_DIR = targetDirectory + "/obj";
 		context.OUTPUT_DIR = targetDirectory;
 		context.ANDROID_INSTALL_LOCATION = project.config.getString("android.install-location", "auto");
-		context.ANDROID_MINIMUM_SDK_VERSION = project.config.getInt("android.minimum-sdk-version", 23);
+		context.ANDROID_MINIMUM_SDK_VERSION = project.config.getInt("android.minimum-sdk-version", 24);
 		context.ANDROID_TARGET_SDK_VERSION = project.config.getInt("android.target-sdk-version", 35);
 		context.ANDROID_EXTENSIONS = project.config.getArrayString("android.extension");
 		context.ANDROID_PERMISSIONS = project.config.getArrayString("android.permission", [
