170896d29b69981cd9fe4c8e2a7a1e4c91e6603f
KarimAkra <karimakra512@gmail.com>
2024-12-12 10:09:50 +0200

    Merge branch 'dev-funkin-arm64' of github.com:FunkinDroidTeam/lime into dev-funkin-arm64

--- src/lime/tools/Asset.hx
+++ src/lime/tools/Asset.hx
@@ -20,6 +20,9 @@ class Asset
 	public var targetPath:String;
 	public var deliveryPackName:String;
 	public var type:AssetType;
+	public var padDelivery:Null<Bool>;
+	public var padMode:String;
+	public var padName:String;
 
 	public function new(path:String = "", rename:String = "", type:AssetType = null, embed:Null<Bool> = null, setDefaults:Bool = true)
 	{
@@ -115,6 +118,9 @@ class Asset
 		asset.targetPath = targetPath;
 		asset.deliveryPackName = deliveryPackName;
 		asset.type = type;
+		asset.padDelivery = padDelivery;
+		asset.padMode = padMode;
+		asset.padName = padName;
 		return asset;
 	}
 }
