6927a998bce178c50ee11a1c00b52718c1fd08a4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-26 23:43:05 +0200

    Improve fullscreen handling for iOS, Android, and desktop platforms
    This also fixes an issue on iOS not making it able to make the line bar be dimmed.

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -28,9 +28,7 @@ namespace lime {
 	SDL_Cursor* SDLCursor::waitCursor = 0;
 	SDL_Cursor* SDLCursor::waitArrowCursor = 0;
 
-	#if defined (IPHONE) || defined (APPLETV)
-	static bool displayModeSet = true;
-	#else
+	#if !defined (IPHONE) && !defined (APPLETV) && !defined (ANDROID)
 	static bool displayModeSet = false;
 	#endif
 
@@ -44,11 +42,18 @@ namespace lime {
 		contextHeight = 0;
 
 		currentApplication = application;
+
 		this->flags = flags;
 
 		int sdlWindowFlags = 0;
 
-		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= displayModeSet ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_FULLSCREEN_DESKTOP;
+		#if defined (IPHONE) || defined (APPLETV) || defined (ANDROID)
+		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN;
+		#else
+		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		#endif
+
+		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
 		if (flags & WINDOW_FLAG_RESIZABLE) sdlWindowFlags |= SDL_WINDOW_RESIZABLE;
 		if (flags & WINDOW_FLAG_BORDERLESS) sdlWindowFlags |= SDL_WINDOW_BORDERLESS;
 		if (flags & WINDOW_FLAG_HIDDEN) sdlWindowFlags |= SDL_WINDOW_HIDDEN;
@@ -73,7 +78,7 @@ namespace lime {
 		}
 		#endif
 
-		#if !defined(EMSCRIPTEN) && !defined(LIME_SWITCH)
+		#if !defined (EMSCRIPTEN) && !defined (LIME_SWITCH)
 		SDL_SetHint (SDL_HINT_ANDROID_TRAP_BACK_BUTTON, "0");
 		SDL_SetHint (SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
 		SDL_SetHint (SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
@@ -254,7 +259,7 @@ namespace lime {
 
 				}
 
-				#elif defined(IPHONE) || defined(APPLETV)
+				#elif defined (IPHONE) || defined (APPLETV)
 
 				// SDL_SysWMinfo windowInfo;
 				// SDL_GetWindowWMInfo (sdlWindow, &windowInfo);
@@ -951,6 +956,7 @@ namespace lime {
 
 		SDL_DisplayMode mode = { pixelFormat, displayMode->width, displayMode->height, displayMode->refreshRate, 0 };
 
+		#if !defined (IPHONE) && !defined (APPLETV) && !defined (ANDROID)
 		if (SDL_SetWindowDisplayMode (sdlWindow, &mode) == 0) {
 
 			displayModeSet = true;
@@ -962,13 +968,16 @@ namespace lime {
 			}
 
 		}
-
+		#else
+		SDL_SetWindowDisplayMode (sdlWindow, &mode);
+		#endif
 	}
-
 
 	bool SDLWindow::SetFullscreen (bool fullscreen) {
 
 		if (fullscreen) {
+
+			#if !defined (IPHONE) && !defined (APPLETV) && !defined (ANDROID)
 
 			if (displayModeSet) {
 
@@ -979,6 +988,12 @@ namespace lime {
 				SDL_SetWindowFullscreen (sdlWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
 			}
+
+			#else
+
+			SDL_SetWindowFullscreen (sdlWindow, SDL_WINDOW_FULLSCREEN);
+
+			#endif
 
 		} else {
 
