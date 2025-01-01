d674370d89e1cb943571209b07188033b9ddd8c3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-01-01 21:30:39 +0000

    Fix Full immersive mode in Android 9 and higher.
    https://github.com/libsdl-org/SDL/commit/89977d0d162270a25774c60ea6ca291e7ecda656

--- templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
+++ templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
@@ -162,8 +162,6 @@ public class GameActivity extends SDLActivity {
 
 			getWindow ().addFlags (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
-			getWindow ().getAttributes ().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-
 		}
 
 		super.onCreate (state);
