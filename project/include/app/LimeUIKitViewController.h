2a8993998523b899797dd2c88ba2c91dabb0235f
KarimAkra <karimakra512@gmail.com>
2025-01-07 23:47:26 +0200

    add missing includes and imports

--- project/include/app/LimeUIKitViewController.h
+++ project/include/app/LimeUIKitViewController.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 #import "../../lib/sdl/src/video/uikit/SDL_uikitviewcontroller.h"
+#import "../../lib/sdl/src/video/uikit/SDL_uikitwindow.h"
 #include "SDL_video.h"
+#include "SDL.h"
 
 @interface LimeUIKitViewController : SDL_uikitviewcontroller
 
