fa422e4aacd479a3b5df225187f29764cb8a198d
KarimAkra <karimakra512@gmail.com>
2025-01-07 21:21:01 +0200

    Attempt to use a custom UIKitViewController for iOS

---
+++ project/include/app/LimeUIKitViewController.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+#import "video/uikit/SDL_uikitviewcontroller.h"
+#include "SDL_video.h"
+
+@interface LimeUIKitViewController : SDL_uikitviewcontroller
+
++ (void)setViewControllerForWindow:(SDL_Window *)window;
+#if !TARGET_OS_TV
+- (UIRectEdge)preferredScreenEdgesDeferringSystemGestures;
+- (BOOL)prefersHomeIndicatorAutoHidden;
+#endif
+
+@end
---
+++ project/src/app/LimeUIKitViewController.m
@@ -0,0 +1,36 @@
+#import "app/LimeUIKitViewController.h"
+
+@implementation LimeUIKitViewController
+
++ (void)setViewControllerForWindow:(SDL_Window *)window {
+    #if !TARGET_OS_TV
+    if (!window || !window->driverdata) {
+      return;
+    }
+
+    SDL_WindowData *data = (__bridge SDL_WindowData *)window->driverdata;
+    data->viewcontroller = [[LimeUIKitViewController alloc] init];
+    #endif
+}
+
+#if !TARGET_OS_TV
+- (UIRectEdge)preferredScreenEdgesDeferringSystemGestures {
+    if (self.homeIndicatorHidden == 3)
+    {
+      return UIRectEdgeBottom;
+    }
+
+    return [super preferredScreenEdgesDeferringSystemGestures];
+}
+
+- (BOOL)prefersHomeIndicatorAutoHidden {
+    if (self.homeIndicatorHidden == 3)
+    {
+      return YES;
+    }
+
+    return [super prefersHomeIndicatorAutoHidden];
+};
+#endif
+
+@end
